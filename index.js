85a41ed0968345bc13af2c727eec95b3bc024530
Mykolas <>
2022-03-25 12:39:42 +0200

    Draft music genre algorithm

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 const express = require("express");
+const brain = require("brain.js");
 const app = express();
 
 app.get("/track/:trackId/genre-details", (_req, res) => {
-  res.json({
-    rock: 0.5,
-    pop: 0.75,
-    rnb: 0.62,
-    edm: 0.3,
+  const audioFeatures = {
+    acousticness: 0.00242,
+    analysis_url:
+      "https://api.spotify.com/v1/audio-analysis/2takcwOaAZWiXQijPHIx7B\n",
+    danceability: 0.585,
+    duration_ms: 237040,
+    energy: 0.842,
+    id: "2takcwOaAZWiXQijPHIx7B",
+    instrumentalness: 0.00686,
+    key: 9,
+    liveness: 0.0866,
+    loudness: -5.883,
+    mode: 0,
+    speechiness: 0.0556,
+    tempo: 118.211,
+    time_signature: 4,
+    track_href: "https://api.spotify.com/v1/tracks/2takcwOaAZWiXQijPHIx7B\n",
+    type: "audio_features",
+    uri: "spotify:track:2takcwOaAZWiXQijPHIx7B",
+    valence: 0.428,
+  };
+
+  const net = new brain.NeuralNetwork({
+    binaryThresh: 0.5,
+    hiddenLayers: [3],
+    activation: "sigmoid",
+    leakyReluAlpha: 0.01,
   });
+
+  net.train([{ input: [0.0235], output: [1, 0, 0, 0] }]);
+  net.train([{ input: [0.101], output: [0, 1, 0, 0] }]);
+  net.train([{ input: [0.0849], output: [0, 0, 1, 0] }]);
+  net.train([{ input: [0.141], output: [0, 0, 0, 1] }]);
+
+  res.json(
+    net.run([audioFeatures.acousticness]).reduce((details, value, index) => {
+      switch (index) {
+        case 0:
+          return { ...details, rock: value };
+        case 1:
+          return { ...details, pop: value };
+        case 2:
+          return { ...details, rnb: value };
+        case 3:
+          return { ...details, edm: value };
+        default:
+          throw new Error("No such genre");
+      }
+    }, {})
+  );
 });
 
 app.use("/", express.static("public"));
--- index.test.js
+++ index.test.js
@@ -6,10 +6,8 @@ it("should accept Spotify track ID and return genre details", async () => {
   const response = await request("http://localhost:3000")
     .get("/track/11dFghVXANMlKmJXsNCbNl/genre-details")
     .expect(200);
-  expect(response.body).toStrictEqual({
-    rock: 0.5,
-    pop: 0.75,
-    rnb: 0.62,
-    edm: 0.3,
-  });
+  expect(response.body.edm).toBeCloseTo(0.9, 1);
+  expect(response.body.pop).toBeCloseTo(0.05, 1);
+  expect(response.body.rnb).toBeCloseTo(0.08, 1);
+  expect(response.body.rock).toBeCloseTo(0.04, 1);
 });
