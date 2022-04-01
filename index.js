10e2584f35d4eb2a9e01fb88995e2c23a3566fd3
Mykolas <>
2022-04-01 11:44:33 +0300

    Provide more input to neural network

--- index.js
+++ index.js
@@ -23,19 +23,75 @@ app.get("/track/:trackId/genre-details", (req, res) => {
 
       const net = new brain.NeuralNetwork({
         binaryThresh: 0.5,
-        hiddenLayers: [3],
         activation: "sigmoid",
-        leakyReluAlpha: 0.01,
+        inputSize: 7,
+        inputRange: 7,
+        hiddenLayers: [7, 7],
+        // outputSize: 7,
+        learningRate: 0.01,
+        decayRate: 0.999,
       });
 
-      net.train([{ input: [0.0235], output: [1, 0, 0, 0] }]);
-      net.train([{ input: [0.101], output: [0, 1, 0, 0] }]);
-      net.train([{ input: [0.0849], output: [0, 0, 1, 0] }]);
-      net.train([{ input: [0.141], output: [0, 0, 0, 1] }]);
+      const metallicaEnterSandman = {
+        id: "5sICkBXVmaCQk5aISGR3x1",
+        danceability: 0.577,
+        energy: 0.828,
+        speechiness: 0.0298,
+        acousticness: 0.00213,
+        instrumentalness: 0.0114,
+        liveness: 0.0581,
+        valence: 0.604,
+      };
+
+      const weekndBlindingLights = {
+        id: "0VjIjW4GlUZAMYd2vXMi3b",
+        danceability: 0.514,
+        energy: 0.73,
+        speechiness: 0.0598,
+        acousticness: 0.00146,
+        instrumentalness: 0.0000954,
+        liveness: 0.0897,
+        valence: 0.334,
+      };
+
+      const drakeKnifeTalk = {
+        id: "2BcMwX1MPV6ZHP4tUT9uq6",
+        danceability: 0.849,
+        energy: 0.424,
+        speechiness: 0.324,
+        acousticness: 0.0635,
+        instrumentalness: 0,
+        liveness: 0.0834,
+        valence: 0.153,
+      };
+
+      const aviciiLevels = {
+        id: "5UqCQaDshqbIk3pkhy4Pjg",
+        danceability: 0.584,
+        energy: 0.889,
+        speechiness: 0.0343,
+        acousticness: 0.0462,
+        instrumentalness: 0.828,
+        liveness: 0.309,
+        valence: 0.464,
+      };
+
+      train(net, metallicaEnterSandman, [1, 0, 0, 0]);
+      train(net, weekndBlindingLights, [0, 1, 0, 0.75]);
+      train(net, drakeKnifeTalk, [0, 0, 1, 0]);
+      train(net, aviciiLevels, [0, 0.25, 0, 1]);
 
       res.json(
         net
-          .run([body.audio_features[0].acousticness])
+          .run([
+            body.audio_features[0].danceability,
+            body.audio_features[0].energy,
+            body.audio_features[0].speechiness,
+            body.audio_features[0].acousticness,
+            body.audio_features[0].instrumentalness,
+            body.audio_features[0].liveness,
+            body.audio_features[0].valence,
+          ])
           .reduce((details, value, index) => {
             switch (index) {
               case 0:
@@ -54,6 +110,23 @@ app.get("/track/:trackId/genre-details", (req, res) => {
     }
   );
 });
+
+const train = (net, details, output) => {
+  net.train([
+    {
+      input: [
+        details.danceability,
+        details.energy,
+        details.speechiness,
+        details.acousticness,
+        details.instrumentalness,
+        details.liveness,
+        details.valence,
+      ],
+      output,
+    },
+  ]);
+};
 
 app.use("/", express.static("public"));
 
