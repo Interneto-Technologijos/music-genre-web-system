dec5045f236211b1bf62582975467fa1300ee855
Mykolas <>
2022-04-08 09:48:26 +0300

    Refresh Spotify token automatically

--- back-end/server.js
+++ back-end/server.js
@@ -8,7 +8,9 @@ require("./track/genre-details")(app);
 
 app.use("/", express.static("public"));
 
-const server = app.listen(process.env.PORT || 3000);
+const server = app.listen(process.env.PORT || 3000, () =>
+  console.log("http://localhost:3000")
+);
 
 module.exports = (cb) => {
   server.close(cb);
--- back-end/spotify/index.js
+++ back-end/spotify/index.js
@@ -1,11 +1,43 @@
 const request = require("request");
 
-module.exports.getAudioFeaturesByTrackId = (trackId, cb) => {
+let accessToken = null;
+
+const getAccessToken = (cb) => {
+  request.post(
+    {
+      url: "https://accounts.spotify.com/api/token",
+      headers: {
+        Authorization:
+          "Basic " +
+          new Buffer(
+            process.env.SPOTIFY_CLIENT_ID +
+              ":" +
+              process.env.SPOTIFY_CLIENT_SECRET
+          ).toString("base64"),
+      },
+      form: {
+        grant_type: "refresh_token",
+        refresh_token: process.env.SPOTIFY_REFRESH_TOKEN,
+      },
+      json: true,
+    },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        cb(new Error(error ? error.message : response.statusCode));
+        return;
+      }
+      console.log(body);
+      cb(null, body.access_token);
+    }
+  );
+};
+
+const getAudioFeatures = (trackId, accessToken, cb) =>
   request.get(
     {
       url: `https://api.spotify.com/v1/audio-features?ids=${trackId}`,
       headers: {
-        Authorization: `Bearer ${process.env.SPOTIFY_ACCESS_TOKEN}`,
+        Authorization: `Bearer ${accessToken}`,
       },
       json: true,
     },
@@ -17,4 +49,49 @@ module.exports.getAudioFeaturesByTrackId = (trackId, cb) => {
       cb(null, body.audio_features[0]);
     }
   );
+
+const refreshTokenAndGetAudioFeatures = (trackId, cb) => {
+  getAccessToken((error, newAccessToken) => {
+    if (error) {
+      cb(error);
+      return;
+    }
+    accessToken = newAccessToken;
+    getAudioFeatures(trackId, accessToken, (error, audioFeatures) => {
+      if (error) {
+        cb(error);
+        return;
+      }
+      cb(null, audioFeatures);
+    });
+  });
+};
+
+module.exports.getAudioFeaturesByTrackId = (trackId, cb) => {
+  if (!accessToken) {
+    refreshTokenAndGetAudioFeatures(trackId, (error, audioFeatures) => {
+      if (error) {
+        cb(error);
+        return;
+      }
+      cb(null, audioFeatures);
+    });
+    return;
+  }
+  getAudioFeatures(trackId, accessToken, (error, audioFeatures) => {
+    if (error) {
+      if (error.message === 401) {
+        refreshTokenAndGetAudioFeatures(trackId, (error, audioFeatures) => {
+          if (error) {
+            cb(error);
+            return;
+          }
+          cb(null, audioFeatures);
+        });
+      }
+      cb(error);
+      return;
+    }
+    cb(null, audioFeatures);
+  });
 };
